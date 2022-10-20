5aca616054acdd9a2895183782baccff653e75a3
JulianaAparicio <juliana_aparicio@outlook.com>
2022-10-19 22:41:22 -0300

    searchAll method added

--- BackEnd/booking/booking/src/main/java/digital/booking/services/CategoryService.java
+++ BackEnd/booking/booking/src/main/java/digital/booking/services/CategoryService.java
@@ -3,13 +3,20 @@ package digital.booking.services;
 import digital.booking.entities.Category;
 import digital.booking.exceptions.NotFoundException;
 import digital.booking.interfaces.IService;
+import digital.booking.repositories.CategoryRepository;
+import org.apache.log4j.Logger;
 
 import java.util.List;
 
 public class CategoryService implements IService<Category> {
+    private final Logger logger = Logger.getLogger(CategoryService.class);
+    private CategoryRepository categoryRepository;
+
     @Override
     public List<Category> searchAll() {
-        return null;
+        logger.debug("Searching all categories...");
+        return categoryRepository.findAll();
+        // Pendiente: agregar una exception (ver si de service) en caso de que no haya categorias.
     }
 
     @Override
