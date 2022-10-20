d0bc2c5c8d9a6f3d0ca78c22a8da58d461df8fed
JulianaAparicio <juliana_aparicio@outlook.com>
2022-10-20 18:55:33 -0300

    searchById method added

--- BackEnd/booking/booking/src/main/java/digital/booking/services/CategoryService.java
+++ BackEnd/booking/booking/src/main/java/digital/booking/services/CategoryService.java
@@ -5,9 +5,12 @@ import digital.booking.exceptions.NotFoundException;
 import digital.booking.interfaces.IService;
 import digital.booking.repositories.CategoryRepository;
 import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class CategoryService implements IService<Category> {
     private final Logger logger = Logger.getLogger(CategoryService.class);
     private CategoryRepository categoryRepository;
@@ -21,7 +24,15 @@ public class CategoryService implements IService<Category> {
 
     @Override
     public Category searchById(Long id) throws NotFoundException {
-        return null;
+        // Acá no habría que agregar Jackson library para poder usar el mapper para el Optional?
+        logger.debug("Searching category with id: " + id);
+        Optional<Category> category = categoryRepository.findById(id);
+        if(category.isPresent()){
+            // return mapper.convertValue(category, Category.class) (ej.)
+            return null;
+        } else {
+            throw new NotFoundException("The category with id: "+ id + " doesn´t exists.");
+        }
     }
 
     @Override
