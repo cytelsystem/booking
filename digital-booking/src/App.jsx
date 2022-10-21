ac1d294319655aa7f33d3e8dd32843ba594eee14
nmflorezr <nmflorezr@gmail.com>
2022-10-20 19:23:03 -0500

    Routes setted

--- digital-booking/src/App.jsx
+++ digital-booking/src/App.jsx
@@ -1,10 +1,36 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
+import Apartment from './pages/Apartment/Apartment';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+
+const router = createBrowserRouter([
+   {
+      path: '/',
+      element: <Home />,
+   },
+   {
+      path: '/login',
+      element: <Login />,
+   },
+   {
+      path: '/register',
+      element: <Register />,
+   },
+   {
+      path: '/:apartmentId',
+      element: <Apartment />,
+   },
+]);
 
 function App() {
    return (
       <div id="app">
          {/* here goes the header :) */}
-         <main>{/* routes :) */}</main>
+         <main>
+            <RouterProvider router={router} />
+         </main>
          <Footer>©2022 Digital Booking</Footer>
       </div>
    );
