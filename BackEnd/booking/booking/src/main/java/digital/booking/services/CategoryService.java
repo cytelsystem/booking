08a2203f07aaf267c7504be8388a02742133607a
JulianaAparicio <juliana_aparicio@outlook.com>
2022-10-20 23:39:38 -0300

    Added mapper, CategoryService's methods. Modified method's names. Added save method.

--- BackEnd/booking/booking/src/main/java/digital/booking/services/CategoryService.java
+++ BackEnd/booking/booking/src/main/java/digital/booking/services/CategoryService.java
@@ -1,11 +1,16 @@
 package digital.booking.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import digital.booking.entities.Category;
+import digital.booking.exceptions.BadRequestException;
 import digital.booking.exceptions.NotFoundException;
 import digital.booking.interfaces.IService;
 import digital.booking.repositories.CategoryRepository;
 import org.apache.log4j.Logger;
+import org.hibernate.service.spi.ServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,40 +18,66 @@ import java.util.Optional;
 @Service
 public class CategoryService implements IService<Category> {
     private final Logger logger = Logger.getLogger(CategoryService.class);
-    private CategoryRepository categoryRepository;
+    @Autowired
+    private final CategoryRepository categoryRepository;
 
-    @Override
-    public List<Category> searchAll() {
-        logger.debug("Searching all categories...");
-        return categoryRepository.findAll();
-        // Pendiente: agregar una exception (ver si de service) en caso de que no haya categorias.
+    @Autowired
+    ObjectMapper mapper;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
     }
 
     @Override
-    public Category searchById(Long id) throws NotFoundException {
-        // Acá no habría que agregar Jackson library para poder usar el mapper para el Optional?
-        logger.debug("Searching category with id: " + id);
-        Optional<Category> category = categoryRepository.findById(id);
-        if(category.isPresent()){
-            // return mapper.convertValue(category, Category.class) (ej.)
-            return null;
+    public List<Category> searchAllCategories() throws ServiceException {
+        logger.debug("Searching all categories...");
+        List<Category> categories = categoryRepository.findAll();
+        if (categories.isEmpty()){
+            throw new ServiceException("There are no categories to list.");
         } else {
-            throw new NotFoundException("The category with id: "+ id + " doesn´t exists.");
+            return categories;
         }
     }
 
     @Override
-    public Category save(Category entity) {
-        return null;
+    public Category searchCategoryById(Long id) throws NotFoundException {
+        logger.debug("Searching category with id: " + id);
+        Optional<Category> category = categoryRepository.findById(id);
+        if(category.isPresent()){
+            return mapper.convertValue(category, Category.class);
+        } else {
+            throw new NotFoundException("The category with id: "+ id + " does not exists.");
+        }
     }
 
     @Override
-    public Category update(Category entity) {
-        return null;
+    public void createCategory(Category category) {
+        logger.debug("Creating new category...");
+        categoryRepository.save(category);
     }
 
     @Override
-    public Boolean delete(Long id) {
-        return null;
+    public void updateCategory(Category category) throws BadRequestException {
+        if (category.getId() == null) {
+            throw new BadRequestException("The category you are trying to modify does not exist.");
+        } else {
+            logger.debug("Updating category...");
+            saveCategory(category);
+        }
+    }
+
+    private void saveCategory(Category category) {
+        logger.debug("Saving category...");
+        categoryRepository.save(category);
+    }
+
+    @Override
+    public void deleteCategory(Long id) throws NotFoundException {
+        if (categoryRepository.findById(id).isEmpty()){
+            throw new NotFoundException("The category with id: "+ id + " does not exists.");
+        } else {
+            logger.debug("Deleting category with id: " + id);
+            categoryRepository.delete(categoryRepository.findById(id).get());
+        }
     }
 }
