f9d81a2503df22634aa1ebe6a7bc2ee5a7e6685d
nmflorezr <nmflorezr@gmail.com>
2022-10-21 18:45:17 -0500

    General view complete

--- digital-booking/src/App.jsx
+++ digital-booking/src/App.jsx
@@ -1,28 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
-import Apartment from './pages/Apartment/Apartment';
-import Home from './pages/Home/Home';
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
-
-const router = createBrowserRouter([
-   {
-      path: '/',
-      element: <Home />,
-   },
-   {
-      path: '/login',
-      element: <Login />,
-   },
-   {
-      path: '/register',
-      element: <Register />,
-   },
-   {
-      path: '/:apartmentId',
-      element: <Apartment />,
-   },
-]);
+import { router } from './core/Routes';
 
 function App() {
    return (
--- digital-booking/src/pages/Home/Home.jsx
+++ digital-booking/src/pages/Home/Home.jsx
@@ -1,5 +1,83 @@
+import { useCallback, useRef, useState } from 'react';
+import Button from './../../shared/Button/Button';
+import PlaceSuggestion from './components/PlaceSuggestion/PlaceSuggestion';
+import './Home.scss';
+
+const places = [
+   {
+      city: 'San Carlos de Bariloche',
+      country: 'Argentina',
+   },
+   {
+      city: 'Buenos Aires',
+      country: 'Argentina',
+   },
+   {
+      city: 'Mendoza',
+      country: 'Argentina',
+   },
+   {
+      city: 'Cordoba',
+      country: 'Argentina',
+   },
+];
+
 const Home = () => {
-   return <div>Home</div>;
+   const [placesShown, setPlaces] = useState(places);
+   const [selectedPlace, setSelect] = useState('¿A donde vamos?');
+
+   const buttonsContainer = useRef(null);
+
+   const handleClick = () => {
+      console.log(val);
+   };
+
+   const handleInputChange = e => {
+      setPlaces(
+         places.filter(place => {
+            const string = e.target.value.toLowerCase();
+            return (
+               place.city.toLowerCase().includes(string) ||
+               place.country.toLowerCase().includes(string)
+            );
+         })
+      );
+   };
+
+   return (
+      <>
+         <section className="searcher">
+            <h1>Busca ofertas en hoteles, casas y mucho mas</h1>
+            <div className="inputs">
+               <input
+                  onFocus={() => buttonsContainer.current.classList.remove('closed')}
+                  onBlur={() => buttonsContainer.current.classList.add('closed')}
+                  onChange={handleInputChange}
+                  type="text"
+                  className="input inputField"
+                  placeholder={selectedPlace}
+               />
+               <div ref={buttonsContainer} className="optionsContainer closed">
+                  <div className="placeOptions">
+                     {placesShown.map((place, i) => (
+                        <PlaceSuggestion key={i} place={place} setPlace={handleClick} />
+                     ))}
+                  </div>
+               </div>
+               <Button classList={'inputField'}>Check in - Check out</Button>
+               <Button classList={'searchButton db-button-primary'}>Buscar</Button>
+            </div>
+         </section>
+         <section className="types">
+            <h2>Buscar por tipo de alojamiento</h2>
+            <article className="typeCards"></article>
+         </section>
+         <section className="recomendations">
+            <h2>Recomendaciones</h2>
+            <article className="typeCards"></article>
+         </section>
+      </>
+   );
 };
 
 export default Home;
