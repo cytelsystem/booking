570ba995a79b4e3c7397d4a88dfdab729d8e2b7e
JulianaAparicio <juliana_aparicio@outlook.com>
2022-10-22 12:40:53 -0300

    Changed Return type of delete method.

--- BackEnd/booking/booking/src/main/java/digital/booking/interfaces/IService.java
+++ BackEnd/booking/booking/src/main/java/digital/booking/interfaces/IService.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface IService<T> {
     List<T> searchAll();
     T searchById(Long id) throws NotFoundException;
-    T create(T entity);
+    T create(T entity) throws BadRequestException;
     T update(T entity) throws BadRequestException;
-    Boolean delete(Long id) throws NotFoundException;
+    void delete(Long id) throws NotFoundException;
 }
