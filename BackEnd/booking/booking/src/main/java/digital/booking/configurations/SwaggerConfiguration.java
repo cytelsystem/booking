e89f4585c3e248c07dfe39806d6690629586e85d
JulianaAparicio <juliana_aparicio@outlook.com>
2022-10-22 12:42:35 -0300

    Commented in block Swagger Configuration

--- BackEnd/booking/booking/src/main/java/digital/booking/configurations/SwaggerConfiguration.java
+++ BackEnd/booking/booking/src/main/java/digital/booking/configurations/SwaggerConfiguration.java
@@ -1,14 +1,15 @@
 package digital.booking.configurations;
-
+/*
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;*/
 
 
+/*
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
@@ -20,4 +21,4 @@ public class SwaggerConfiguration {
                 .paths(PathSelectors.any())
                 .build();
     }
-}
+}*/
