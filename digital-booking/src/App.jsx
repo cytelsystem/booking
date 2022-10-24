adb1ad6cf093ce4ff40892776afe4af0d097ea01
Camilo  Martinez <camilo_2510@hotmail.com>
2022-10-24 14:10:36 -0500

    removing code unnecessary

--- digital-booking/src/App.jsx
+++ digital-booking/src/App.jsx
@@ -1,47 +1,8 @@
-import { useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
-import ProductCard from './components/ProductCard/ProductCard';
-import Card from './shared/Card/Card';
-import FacebookIcon from './shared/Icons/FacebookIcon';
-import PoolIcon from './shared/Icons/PoolIcon';
-import WifiIcon from './shared/Icons/WifiIcon';
-import Input from './shared/Input/Input';
-import Typehead from './shared/Input/Typehead';
-import { getValidations } from './utils/formValidations';
-import { getValidationErrors } from './utils/validationErrors';
 import { router } from './core/Routes';
 
 function App() {
-   const [inputValue, setInputValue] = useState(null);
-   const [typeheadValue, setTypeheadValue] = useState(null);
-
-   const product = {
-      image: {
-         url: 'https://construccionesprisma.com.co/images/apartment_photos/22_41_pradoalto97.5m201.jpg',
-         productName: 'apto1',
-      },
-      info: {
-         title: 'Title',
-         category: 'Departamentos',
-         points: 8,
-         textRate: 'muy bueno',
-         distance: 'A 900 m del centro',
-         amenities: [
-            {
-               icon: <WifiIcon />,
-               label: 'Wi-Fi',
-            },
-            {
-               icon: <PoolIcon />,
-               label: 'Pool Service',
-            },
-         ],
-         description:
-            'En el corazón de San Telmo, disfruta de un albergue inspirado en las pasiones de Buenos Aires.',
-      },
-   };
-
    return (
       <div id="app">
          <main>
