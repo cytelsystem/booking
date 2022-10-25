82d7738b56fad568747789bb31bc7bfe7faeca04
Hector Javier Moreno <cytelsystem@gmail.com>
2022-10-25 15:37:00 -0500

    Se adiciono dependencia den el pom Swagger, eliminacion carpeta swagerConfiguration, adicion cambio controller swagger, cambio direccion AWS aplication.propiertiesy comentar aplication-h2.propierties

--- BackEnd/booking/booking/src/main/java/digital/booking/controllers/CategoryController.java
+++ BackEnd/booking/booking/src/main/java/digital/booking/controllers/CategoryController.java
@@ -4,6 +4,8 @@ import digital.booking.DTO.CategoryDTO;
 import digital.booking.exceptions.BadRequestException;
 import digital.booking.exceptions.NotFoundException;
 import digital.booking.services.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import net.bytebuddy.build.Plugin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,26 +19,30 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @Operation(summary = "Consultar Todas las categorias")
     @GetMapping
     public ResponseEntity<List<CategoryDTO>> findAllCategories(){
         return ResponseEntity.ok(categoryService.searchAll());
     }
 
+    @Operation(summary = "Consultar Categoria por ID")
     @GetMapping("/{id}")
     public ResponseEntity<CategoryDTO> findCategoryById(@PathVariable Long id) throws NotFoundException {
         return ResponseEntity.ok(categoryService.searchById((id)));
     }
-
+    @Operation(summary = "Crear nueva categoria")
     @PostMapping
     public ResponseEntity<CategoryDTO> createCategory(@RequestBody CategoryDTO category) throws BadRequestException {
         return ResponseEntity.ok(categoryService.create(category));
     }
 
+    @Operation(summary = "Actualizar categoria por ID")
     @PutMapping("/{id}")
     public ResponseEntity<CategoryDTO> updateCategory(@PathVariable Long id, @RequestBody CategoryDTO category) throws NotFoundException {
         return ResponseEntity.ok(categoryService.update(category, id));
     }
 
+    @Operation(summary = "Eliminar categoria")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long id) throws NotFoundException {
         categoryService.delete(id);
--- BackEnd/booking/booking/src/main/java/digital/booking/entities/Category.java
+++ BackEnd/booking/booking/src/main/java/digital/booking/entities/Category.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor
@@ -16,8 +17,11 @@ public class Category {
     @Id
     @SequenceGenerator(name="category_seg",initialValue = 1,allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "category_seq")
+
+    @NotNull
     private Long id;
 
+    @NotNull
     @Column
     private String title;
 
