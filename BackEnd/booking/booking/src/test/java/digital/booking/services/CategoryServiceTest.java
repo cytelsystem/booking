1fe18a06235f1a32793e338853c593a549a365ff
JulianaAparicio <juliana_aparicio@outlook.com>
2022-10-25 21:19:41 -0300

    Added tests for create and searchById CategoryService methods.

--- BackEnd/booking/booking/src/test/java/digital/booking/services/CategoryServiceTest.java
+++ BackEnd/booking/booking/src/test/java/digital/booking/services/CategoryServiceTest.java
@@ -1,8 +1,13 @@
 package digital.booking.services;
 
-import digital.booking.entities.Category;
+import digital.booking.DTO.CategoryDTO;
+import digital.booking.exceptions.BadRequestException;
+import digital.booking.exceptions.NotFoundException;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class CategoryServiceTest {
@@ -10,7 +15,37 @@ public class CategoryServiceTest {
     @Autowired
     CategoryService categoryService;
 
-    Category category1 = new Category();
-    Category category2 = new Category();
+    CategoryDTO categoryDTO = new CategoryDTO(1L,"titleTest","descriptionTest","imageURLTest");
+
+
+    @Test
+    public void testCreate() {
+        try{
+            categoryService.create(categoryDTO);
+
+            CategoryDTO categoryTest = categoryService.searchById(1L);
+            assertNotNull(categoryTest,"The category is null");
+            assertEquals(categoryDTO.getTitle(), categoryTest.getTitle(), "Titles don't match.");
+            assertEquals(categoryDTO.getDescription(), categoryTest.getDescription(), "Descriptions don't match.");
+            assertEquals(categoryDTO.getImageURL(), categoryTest.getImageURL(), "ImageURLs don't match.");
+
+        } catch (BadRequestException | NotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testSearchById(){
+        try {
+            CategoryDTO categoryFounded = categoryService.searchById(categoryDTO.getId());
+            assertEquals(categoryDTO.getId(), categoryFounded.getId(), "Ids don't match.");
+            assertNotNull(categoryFounded,"The categpry founded is null.");
+
+        } catch (NotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
