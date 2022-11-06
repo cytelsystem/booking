b83138a5d75cddfdc03ef64604ea51e02b8055f5
JulianaAparicio <juliana_aparicio@outlook.com>
2022-11-06 15:29:42 -0300

    Fixed Query HQL sintaxis

--- BackEnd/booking/booking/src/main/java/digital/booking/repositories/FavoriteRepository.java
+++ BackEnd/booking/booking/src/main/java/digital/booking/repositories/FavoriteRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
-    @Query(value = "SELECT * FROM favorites f INNER JOIN users u ON f.id = u.id")
+    @Query(value = "SELECT f.* FROM favorites f INNER JOIN users u ON f.id = u.id")
     List<Favorite>findFavoritesByUser(Long id);
 }
